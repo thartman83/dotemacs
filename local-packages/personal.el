b49ffa5ed69c538237c2318806c4106074c15a99
Thomas Hartman <rokstar83@gmail.com>
2013-10-01 12:30:36 -0400

    Added noflet as an external package. Updated the dotemacs file. Also added appropriate comments and what have you to personal.el

--- local-packages/personal.el
+++ local-packages/personal.el
@@ -1,5 +1,26 @@
-;; personal.el --- personal information
-(defvar *full-name* "Tom Hartman")
+;;; personal.el --- Personal Information
+
+;; Copyright (c) 2013 Thomas Hartman (rokstar83@gmail.com)
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or the License, or (at your option) any later
+;; version.
+
+;; This program is distributed in the hope that it will be useful
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(defvar *full-name* "Thomas Hartman")
 (defvar *email* "rokstar83@gmail.com")
 
 (provide 'personal)
+;;; personal.el ends here
